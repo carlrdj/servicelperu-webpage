d8eadd9acec237d9add125a1e63e2e503e2b5d09
Carl RDJ <carl.rdj@gmail.com>
2020-03-31 22:44:26 -0500

    test php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Servicel</title>
     <!-- CSS here -->
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/owl.carousel.min.css">
