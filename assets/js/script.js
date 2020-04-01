2598ab081f4a4d281c011a6f81d1d22868839cd5
Nerio DM <nerioduranmendoza.anonimus8@gmail.com>
2020-04-01 11:42:23 -0500

    cambios de imagenes menos pesados y footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var header = document.getElementById("header")
+var footer = document.getElementById("footer")
+
 header.innerHTML = `
         <div class="header-area ">
             <div id="sticky-header" class="main-header-area">
@@ -129,4 +131,40 @@ header.innerHTML = `
                 </div>
             </div>
         </div>
-    `
+    `;
+
+    footer.innerHTML = `<div class="footer_top">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-2 col-md-3">
+                <div class="footer_logo wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">
+                    <a href="index.html">
+                        <img src="./assets/img/logo.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-7 col-lg-7 col-md-9">
+                <div class="menu_links">
+                    <ul>
+                        <li><a class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".2s" href="#">About</a class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s"></li>
+                        <li><a class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".4s" href="#">Services</a></li>
+                        <li><a class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".6s" href="#">Portfolio</a></li>
+                        <li><a class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".8s" href="#">Pages</a></li>
+                        <li><a class="wow fadeInDown" data-wow-duration="1s" data-wow-delay="1s" href="#">Blog</a></li>
+                        <li><a class="wow fadeInDown" data-wow-duration="1s" data-wow-delay="1.1s" href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-12">
+                <div class="socail_links">
+                    <ul>
+                        <li><a class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s" href="#"> <i class="fa fa-facebook"></i> </a></li>
+                        <li><a class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s" href="#"> <i class="fa fa-twitter"></i> </a></li>
+                        <li><a class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s" href="#"> <i class="fa fa-instagram"></i> </a></li>
+                        <li><a class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s" href="#"> <i class="fa fa-google-plus"></i> </a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
